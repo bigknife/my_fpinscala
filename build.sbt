a82463ca2ce9351389d86bc890c908c1d5fe1f02
bigknife <bigknife@outlook.com>
2017-10-08 22:36:41 +0800

    stream code

---
+++ build.sbt
@@ -0,0 +1,39 @@
+import Dependencies._
+import common._
+
+scalaVersion in ThisBuild := "2.11.8"
+scalacOptions in ThisBuild ++= Seq(
+  "-target:jvm-1.7",
+  "-encoding",
+  "UTF-8",
+  "-unchecked",
+  "-deprecation",
+  "-Xfuture",
+  "-feature",
+  "-language:higherKinds",
+  "-language:implicitConversions",
+  "-Ywarn-dead-code",
+  "-Ywarn-numeric-widen",
+  "-Ywarn-value-discard",
+  "-Ywarn-unused"
+)
+
+scalacOptions in (Compile, console) ~= (_ filterNot (_ contains "paradise"))
+
+// customize version
+val gitHeadCommitSha = settingKey[String]("current git commit SHA")
+val release          = settingKey[Boolean]("is publishing released version")
+release := sys.props("release") == "true"
+
+gitHeadCommitSha in ThisBuild := Process("git rev-parse HEAD").lines.head
+
+version in ThisBuild := {
+  val v = "0.1.0"
+  if (release.value) v
+  else s"$v-${gitHeadCommitSha.value}-SNAPSHOT"
+}
+
+initialCommands in console := """import fpinscala._"""
+
+// client project
+lazy val rootProject = simpleProject
---
+++ src/main/scala/fpinscala/Main.scala
@@ -0,0 +1,85 @@
+package fpinscala
+
+
+sealed trait Stream[+A] {
+  import Stream._
+
+  def uncons: Option[(A, Stream[A])]
+  def isEmpty: Boolean = uncons.isEmpty
+
+  def toList: List[A] = {
+    if (isEmpty) List.empty[A]
+    else {
+      val (head, tail) = uncons.get
+
+      def go(acc: List[A], hd: A, tl: Stream[A]): List[A] = {
+        val now = acc :+ hd
+        tl.uncons match {
+          case Some((hd1, tl1)) => go(now, hd1, tl1)
+          case None => now
+        }
+      }
+      go(List.empty[A], head, tail)
+    }
+  }
+
+  def take(n: Int): Stream[A] =
+    if (n <= 0) empty[A]
+    else
+      uncons match {
+        case None => empty[A]
+        case Some((head, tail)) =>
+          cons(head, tail.take(n -1))
+      }
+
+  def takeWhile(p: A => Boolean): Stream[A] =
+    if (isEmpty) empty[A]
+    else {
+      val (head, tail) = uncons.get
+      if (p(head)) cons(head, tail.takeWhile(p))
+      else tail.takeWhile(p)
+    }
+
+  def foldRight[B](z: => B)(f: (A, => B) => B): B =
+    uncons match {
+      case Some((head, tail)) =>
+        //println(s"foldRight $head")
+        f(head, tail.foldRight(z)(f))
+      case None => z
+    }
+
+  def exists(p: A => Boolean): Boolean =
+    foldRight(false) {(next, acc) =>
+      p(next) || acc
+    }
+}
+
+object Stream {
+  def empty[A] = new Stream[A] {
+    def uncons: Option[(A, Stream[A])] = None
+  }
+
+  def cons[A](head: A, tail: => Stream[A]): Stream[A] =
+    new Stream[A] {
+      lazy val uncons = Some((head, tail))
+    }
+
+  def apply[A](as: A*): Stream[A] =
+    if (as.isEmpty) empty[A]
+    else cons(as.head, apply(as.tail:_*))
+}
+
+object Main extends App {
+  println("hello,world")
+
+  val s = Stream[Int]((1 to 1000000): _*)
+  val s1 = s.take(10000)
+  val s2 = s.takeWhile(_ % 2 == 0)
+
+ 
+
+  println(s.toList)
+  println(s1.toList)
+  println(s2.toList)
+
+}
