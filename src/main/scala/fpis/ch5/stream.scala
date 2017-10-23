96d00cfdd7709612b83675449151c88173bfb669
bigknife <bigknife@outlook.com>
2017-10-23 22:07:54 +0800

    add stream

--- src/main/scala/fpis/ch5/stream.scala
+++ src/main/scala/fpis/ch5/stream.scala
@@ -31,8 +31,23 @@ object Stream {
       go(Nil, stream)
     }
 
-    def take(n: Int): Stream[A] = stream match {
-      
+    def take(n: Int): Stream[A] = stream.uncons match {
+      case None => empty
+      case Some((hd, tail)) if n <= 0 => empty
+      case Some((hd, tail)) if n <= 1 => cons(hd, empty)
+      case Some((hd, tail)) => cons(hd, tail.take(n-1))
     }
+
+    def takeWhile(p: A => Boolean): Stream[A] = stream.uncons match {
+      case None => empty
+      case Some((hd, tail)) if p(hd) => cons(hd, tail.takeWhile(p))
+      case Some((hd, tail))  => tail.takeWhile(p)
+    }
+
+    def foldRight[B](z: => B)(f: (A, => B) => B): B =
+      stream.uncons match {
+        case Some((h, t)) => f(h, t.foldRight(z)(f))
+        case None => z
+      }
   }
 }
